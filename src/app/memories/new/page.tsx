1524e68760f0244958aaa47dd31f041c708b213c
Rodrigo Pereira <rodrigopr404@gmail.com>
2023-05-19 17:29:32 -0300

    🚧 Pages construction

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -1,9 +1,49 @@
+'use client'
+import { MediaPicker } from '@/components/MediaPicker'
+import { api } from '@/lib/api'
 import { Camera, ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
+import { FormEvent } from 'react'
+import Cookie from 'js-cookie'
+import { useRouter } from 'next/navigation'
 
 export default function NewMemory() {
+  const router = useRouter()
+  async function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    const fileToUpload = formData.get('coverURL')
+
+    let coverURL = ''
+    if (fileToUpload) {
+      const uploadFormData = new FormData()
+      uploadFormData.set('file', fileToUpload)
+
+      const uploadResponse = await api.post('/upload', uploadFormData)
+
+      coverURL = uploadResponse.data.fuleUrl
+    }
+
+    await api.post(
+      '/memories',
+      {
+        coverURL,
+        content: formData.get('content'),
+        isPublic: formData.get('isPublic'),
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${Cookie.get('token')}`,
+        },
+      },
+    )
+
+    router.push('/')
+  }
   return (
-    <div className="flex flex-1 flex-col gap-4">
+    <div className="flex flex-1 flex-col gap-4  p-16">
       <Link
         href="/"
         className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100"
@@ -12,7 +52,10 @@ export default function NewMemory() {
         voltar à timeline
       </Link>
 
-      <form className="flex flex-1 flex-col gap-2">
+      <form
+        onSubmit={handleCreateMemory}
+        className="flex flex-1 flex-col gap-2"
+      >
         <div className="flex items-center gap-4">
           <label
             htmlFor="media"
@@ -35,7 +78,7 @@ export default function NewMemory() {
             Tornar memória pública
           </label>
         </div>
-        <input type="file" id="media" className="invisible h-0 w-0" />
+        <MediaPicker />
 
         <textarea
           name="content"
@@ -43,6 +86,13 @@ export default function NewMemory() {
           className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
         />
+
+        <button
+          type="submit"
+          className="inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+        >
+          Salvar
+        </button>
       </form>
     </div>
   )
